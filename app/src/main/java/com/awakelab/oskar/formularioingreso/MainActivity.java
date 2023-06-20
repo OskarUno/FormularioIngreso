54c4ebfe8e1721682a106edb265b58e0f8f06035
OskarUno <oorellana80@gmail.com>
2023-06-20 00:33:27 -0400

    Diseño de ingreso de susuario listo

---
+++ app/src/main/java/com/awakelab/oskar/formularioingreso/MainActivity.java
@@ -0,0 +1,14 @@
+package com.awakelab.oskar.formularioingreso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
