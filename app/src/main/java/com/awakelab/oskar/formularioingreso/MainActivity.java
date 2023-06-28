d2c7a01a802d939dac39d90d2d67fb7f4359cb17
OskarUno <oorellana80@gmail.com>
2023-06-27 20:41:22 -0400

    Ejercicio Individual 10: Se habilita binding para vinculacion de las vistas que contengan id. 1.- build.grade -> android { buildFeatures { viewBinding true }.... 2.- MainActivity ->  private ActivityMainBinding binding;  binding = ActivityMainBinding.inflate(getLayoutInflater());  binding.misElementosDeLaPalette.setOnClick........

--- app/src/main/java/com/awakelab/oskar/formularioingreso/MainActivity.java
+++ app/src/main/java/com/awakelab/oskar/formularioingreso/MainActivity.java
@@ -1,36 +1,32 @@
 package com.awakelab.oskar.formularioingreso;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.awakelab.oskar.formularioingreso.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
         initListeners();
     }
 
     private void initListeners() {
-        EditText editTextName = findViewById(R.id.editTextName);
-        EditText editTextLastName = findViewById(R.id.editTextLastName);
-        EditText editTextEmail = findViewById(R.id.editTextEmail);
-        EditText editTextPassword = findViewById(R.id.editTextPassword);
-        Button buttonCreate = findViewById(R.id.buttonCreate);
-
-        buttonCreate.setOnClickListener(new View.OnClickListener() {
+        binding.buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = editTextName.getText().toString().trim();
-                String lastName = editTextLastName.getText().toString().trim();
-                String email = editTextEmail.getText().toString().trim();
-                String pass = editTextPassword.getText().toString().trim();
+                String name = binding.editTextName.getText().toString().trim();
+                String lastName = binding.editTextLastName.getText().toString().trim();
+                String email = binding.editTextEmail.getText().toString().trim();
+                String pass = binding.editTextPassword.getText().toString().trim();
 
                 if(name.isEmpty() || lastName.isEmpty() || email.isEmpty() || pass.isEmpty() || !email.contains("@")){
                     Toast.makeText(getBaseContext(), "Faltan campos por agregar", Toast.LENGTH_SHORT).show();
@@ -40,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 String formulario = "Usuario: " + name + ", LastName: " + lastName + ", Email: " + email + ", Contraseña: " + pass;
 
                 Toast.makeText(getBaseContext(),formulario, Toast.LENGTH_LONG).show();
-
-
             }
         });
 
